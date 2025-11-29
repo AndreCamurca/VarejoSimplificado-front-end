39dc729815c0027822353f69d611a4ac1fbdf766
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-11-28 23:56:09 -0300

    refact: move side menu to component

---
+++ src/pages/dashboard/components/SideMenuComponent/index.tsx
@@ -0,0 +1,80 @@
+import { NavLink } from 'react-router-dom'
+
+import {
+    X,
+    LogOut,
+    type LucideIcon,
+} from 'lucide-react'
+
+
+import styles from './styles.module.css';
+
+interface MenuItem {
+    path: string;
+    label: string;
+    icon: LucideIcon;
+    ariaLabel: string;
+}
+
+const MenuItemComponent = (props: MenuItem) => {
+
+    const {
+        path,
+        label,
+        icon: Icon,
+        ariaLabel
+    } = props;
+
+    return (
+        <li>
+            <NavLink to={path} className={styles.menuLink}>
+                {({isActive}) => (
+                    <button className={`${ styles.itemMenu } ${ isActive ? styles.active : '' }`} aria-label={ariaLabel}>
+                        { Icon && <Icon /> }
+                        <p>
+                            { label }
+                        </p>
+                    </button>
+                )}
+            </NavLink>
+        </li>
+    );
+}
+
+interface SideMenuComponentProps {
+    menuItems: Array<MenuItem>;
+    onCloseMenu: () => void,
+}
+
+
+export const SideMenuComponent = (props: SideMenuComponentProps) => {
+
+    const { 
+        menuItems,
+        onCloseMenu,
+    } = props;
+
+
+    return (
+        <aside className={styles.sideMenu}>
+            <ul className={styles.menu}>
+                <li className={styles.closeMenu}>
+                    <button 
+                        className={styles.menuButton} 
+                        aria-label='Botão para fechar menu'
+                        onClick={onCloseMenu}>
+                            <X />
+                    </button>
+                </li>
+
+                { menuItems.map((item, index) => <MenuItemComponent {...item} key={index} />) }
+            </ul>
+
+            <button className={styles.exitButton}>
+                <LogOut />
+                <p>Sair</p>
+            </button>
+        </aside>
+    );
+
+}
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,76 @@
+import { useCallback, useState } from 'react';
+import { 
+    Menu,
+    User,
+    Package,
+    Home,
+} from 'lucide-react'
+
+import { SideMenuComponent } from './components/SideMenuComponent';
+import styles from "../../styles/dashboard.module.css";
+
+
+
+const menuItems = [
+    { 
+        path: '/dashboard',
+        label: 'Dashboard',
+        icon: Home,
+        ariaLabel: 'Ir para Dashboard'
+    },
+    { 
+        path: '/users', 
+        label: 'Usuarios', 
+        icon: User, 
+        ariaLabel: 'Ir para Pagina de Usuarios' 
+    },
+    { 
+        path: '/products', 
+        label: 'Produtos', 
+        icon: Package, 
+        ariaLabel: 'Ir para Pagina de Usuarios'
+    },
+];
+
+
+
+export const DashboardPage = () => {
+    const [isShowMenu, setIsShowMenu] = useState(false);
+
+    const handlerShowMenu = useCallback(() => {
+        setIsShowMenu(() => true);
+    }, []);
+
+    const handlerHiddenMenu = useCallback(() => {
+        setIsShowMenu(() => false);
+    }, []);
+
+    return (
+        <div className={styles.dashboardContainer}>
+
+            <header className={styles.header}>
+                <button className={styles.menuButton} aria-label='Menu button' onClick={handlerShowMenu}>
+                    <Menu />
+                </button>
+
+                <h2 className={styles.companyName}>
+                    Mercadinho do Edinho
+                </h2>
+
+                <div></div>
+            </header>
+
+            <main className={styles.main}>
+                <div className={`${ styles.menuContainer } ${ !isShowMenu ? styles.hiddenMenu : '' }`}>
+                    <SideMenuComponent 
+                        menuItems={menuItems}
+                        onCloseMenu={handlerHiddenMenu} />
+                </div>
+
+                <div className={styles.mainContent}>
+                    <p>Content</p>
+                </div>
+            </main>
+        </div>
+    );
+}
